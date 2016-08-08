c25cc02e25b48622060422c135d02cd298cd2939
Vasiliy Alferov <ya-ikmik2012@yandex.ru>
2016-08-08 19:43:24 +0300

    many changes

--- new_code/hydro.h
+++ new_code/hydro.h
@@ -6,8 +6,8 @@ using namespace std;
 
 class hydro: public force {
   public:
-    vector<vect> get_velocity_increment(const std::vector<vect>& position);
+    vector<vect> get_velocity_increment(const parameters& params, const std::vector<vect>& position);
     double srat;
     double erat;
     vect mass_center;
-}
+};
