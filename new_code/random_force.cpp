099fe52ffe2b6b46286083f7c5b5e35195e54446
igor_ipm <igor_ipm@localhost>
2016-08-09 16:31:20 +0300

    adding file for macrodefinitions, now is TEST_MODE only

--- new_code/random_force.cpp
+++ new_code/random_force.cpp
@@ -1,4 +1,10 @@
 #include "random_force.h"
+#include "definition.h"
+#include <fstream>
+#include <cassert>
+#include<iostream>
+
+
 
 
 random_force::random_force(unsigned seed) : generator(seed), distribution(0.0, 1.0)
@@ -15,6 +21,22 @@ std::vector<vect> random_force::get_velocity_increment(const parameters& params,
 	std::vector<vect> res;
 
 	vect v(0, 0, 0);
+#ifdef TEST_MODE
+        std::ifstream ifstr;
+        ifstr.open("./normal_distr_500k.txt");
+        double dbuf;
+        for (int i = 0; i < position.size(); ++i)
+        {
+            ifstr>>dbuf;
+            v.x += dbuf*params.mno;
+            ifstr>>dbuf;
+            v.y += dbuf*params.mno;
+            ifstr>>dbuf;
+            v.z += dbuf*params.mno;
+            res.push_back((1 / params.gamma)*v);
+        }
+        ifstr.close();
+#else // TEST_MODE
 	for (int i = 0; i < position.size(); ++i)
 	{
 		v.x += distribution(generator)*params.mno;
@@ -22,5 +44,6 @@ std::vector<vect> random_force::get_velocity_increment(const parameters& params,
 		v.z += distribution(generator)*params.mno;
 		res.push_back((1 / params.gamma)*v);
 	}
+#endif //TEST_MODE
 	return res;
 }
